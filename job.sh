446e0ff1733bb06d16f463a47510b6753d0fd8af
FrancescoPesce <francesco1.pesce@mail.polimi.it>
2024-06-05 18:36:09 +0200

    Change scripts to run the application using SLURM

--- job.sh
+++ job.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+#SBATCH --partition=g100_all_serial ## Partition.
+#SBATCH --job-name=matrixMult ## Name of the job.
+#SBATCH --output=output.txt ## Output file.
+#SBATCH --error=error.txt ## Error file.
+#SBATCH --time=00:00:10 ## Job Duration.
+#SBATCH --nodes=1 ## Number of nodes.
+#SBATCH --ntasks-per-node=2 ## Number of MPI processes.
 
-# Define the path to the Singularity Image File.
+## Load MPI.
+module load openmpi
+
+## Define the path to the Singularity Image File.
 SIF_FILE="myimage.sif"
 
-# Define the output files for stdout and stderr.
-STDOUT_FILE="output.txt"
-STDERR_FILE="error.txt"
-
-# Define the number of MPI processes.
-NUM_PROCESSES=2
-
-# Run the application within the Singularity container using MPI.
-singularity exec "$SIF_FILE" mpirun -np $NUM_PROCESSES main > "$STDOUT_FILE" 2> "$STDERR_FILE"
+## Run the application within the Singularity container using MPI.
+srun singularity exec "$SIF_FILE" main
